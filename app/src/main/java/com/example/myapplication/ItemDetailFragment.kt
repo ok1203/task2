f44569c78c211e80a69aea49b27e0b4d71d98127
User <omar.12200336@gmail.com>
2021-05-31 17:38:31 +0600

    task1

---
+++ app/src/main/java/com/example/myapplication/ItemDetailFragment.kt
@@ -0,0 +1,103 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.myapplication.placeholder.PlaceholderContent
+import com.example.myapplication.databinding.FragmentItemDetailBinding
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.http.GET
+import retrofit2.Call
+
+/**
+ * A fragment representing a single Item detail screen.
+ * This fragment is either contained in a [ItemListFragment]
+ * in two-pane mode (on larger screen devices) or self-contained
+ * on handsets.
+ */
+class ItemDetailFragment : Fragment() {
+
+    /**
+     * The placeholder content this fragment is presenting.
+     */
+
+    private val BASE_URL =
+        "https://api.thingspeak.com/channels/1404059/fields/1.json?results=2"
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .build()
+
+    interface MarsApiService {
+        @GET("")
+        fun getProperties():
+                Call<String>
+    }
+
+    private var item: PlaceholderContent.PlaceholderItem? = null
+
+    lateinit var itemDetailTextView: TextView
+
+    private var _binding: FragmentItemDetailBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            if (it.containsKey(ARG_ITEM_ID)) {
+                // Load the placeholder content specified by the fragment
+                // arguments. In a real-world scenario, use a Loader
+                // to load content from a content provider.
+                item = PlaceholderContent.ITEM_MAP[it.getString(ARG_ITEM_ID)]
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentItemDetailBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+
+        binding.toolbarLayout?.title = item?.content
+
+        itemDetailTextView = binding.itemDetail
+        // Show the placeholder content as text in a TextView.
+        item?.let {
+            itemDetailTextView.text = it.details
+        }
+
+        return rootView
+    }
+
+    companion object {
+        /**
+         * The fragment argument representing the item ID that this fragment
+         * represents.
+         */
+        const val ARG_ITEM_ID = "item_id"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
